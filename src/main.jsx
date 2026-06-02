3f2faa6aaed1b7b4388890b35d003b70e7148c2d
Manoj HP <manojhp584@gmail.com>
2026-06-02 13:36:45 +0530

    toast changed to sooner

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")).render(
 
         <App />
 
-        <Toaster position="bottom-right" richColors />
+        <Toaster position="bottom-right" richColors closeButton />
       </ColorModeProvider>
     </QueryClientProvider>
   </StrictMode>,
